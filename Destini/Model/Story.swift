978b585da9a4a98e12e86f88d4a5bd8e6cf72aee
Rob Ranf <robranf@gmail.com>
2020-06-30 20:14:30 -0700

    first commit

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,9 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Rob Ranf on 6/30/20.
+//  Copyright © 2020 Rob Ranf. All rights reserved.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,9 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Rob Ranf on 6/30/20.
+//  Copyright © 2020 Rob Ranf. All rights reserved.
+//
+
+import Foundation

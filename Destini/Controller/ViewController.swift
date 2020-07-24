d1869fbfac397642c49dae2d5d3571de929e5ea0
Rob Ranf <robranf@gmail.com>
2020-07-23 17:15:57 -0700

    adding button and text, correcting layouts and constraints

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    let story0: String = "You see a fork in the road."
+    let choice1: String = "Take a left."
+    let choice2: String = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: [])
+        choice2Button.setTitle(choice2, for: [])
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
 }
-

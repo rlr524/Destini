32cb984815ddaa342c7071f1ce65501dd13fd508
Rob Ranf <robranf@gmail.com>
2020-07-28 17:53:40 -0700

    Adding more stories and starting branch logic for story

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -13,18 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0: String = "You see a fork in the road."
-    let choice1: String = "Take a left."
-    let choice2: String = "Take a right."
+    let stories = [
+        Story(t: "You see a fork in the road.", c1: "Take a left.", c2: "Take a right."),
+        Story(t: "You see a tiger.", c1: "Shout for help.", c2: "Play dead."),
+        Story(t: "You find a treasure chest.", c1: "Open it.", c2: "Check for traps."),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: [])
-        choice2Button.setTitle(choice2, for: [])
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: [])
+        choice2Button.setTitle(stories[0].choice2, for: [])
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        let response = sender.currentTitle
+        if (response == "Take a left.") {
+            storyLabel.text = stories[1].title
+            choice1Button.setTitle(stories[1].choice1, for: [])
+            choice2Button.setTitle(stories[1].choice2, for: [])
+        } else {
+            storyLabel.text = stories[2].title
+            choice1Button.setTitle(stories[2].choice1, for: [])
+            choice2Button.setTitle(stories[2].choice2, for: [])
+        }
     }
 }
